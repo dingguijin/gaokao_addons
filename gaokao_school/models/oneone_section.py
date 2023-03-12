2f0992011fa3ff5e2199be2dd992aad9da53abf1
Guijin Ding <dingguijin@gmail.com>
2023-03-12 12:02:15 +0800

    province control

--- gaokao_school/models/oneone_section.py
+++ gaokao_school/models/oneone_section.py
@@ -10,7 +10,7 @@ _logger = logging.getLogger(__name__)
 _json_data_dir = os.path.join(os.path.dirname(__file__), "../../json_data")
 _provinces = ["北京", "天津", "河北", "山西", "内蒙古", "辽宁", "吉林", "黑龙江", "上海", "江苏", "浙江", "安徽", "福建", "江西", "山东", "河南", "湖北", "湖南", "广东", "广西", "海南", "重庆", "四川", "贵州", "云南", "西藏", "陕西", "甘肃", "青海",  "宁夏", "新疆"]
 
-class SchoolScore(models.Model):
+class OneoneSection(models.Model):
     _name = 'gaokao.oneone_section'
 
     name = fields.Char('分数')
@@ -29,13 +29,13 @@ class SchoolScore(models.Model):
     def _load_json_file(self, path, year, provinces, types):
     
         _json_file = os.path.basename(path)
-        _ss = _json_file.split("_")
+        _ss = _json_file.split(".")[0].split("_")
         _province_id = int(_ss[1])
         _type_id = int(_ss[2])
         _score_id = _ss[3]
 
         _province_name = provinces[_province_id]
-        _type_name = _types[_type_id]
+        _type_name = types[_type_id]
 
         _records = []
         with open(path) as _file:
@@ -71,6 +71,7 @@ class SchoolScore(models.Model):
         _records = []
         for _j_path in os.walk(_path):
             _json_files = list(_j_path)[2]
+            _logger.info(_json_files)
             for _json_file in _json_files:
                 _records += self._load_json_file(os.path.join(_path, _json_file), year, provinces, types)
         return _records
@@ -93,6 +94,8 @@ class SchoolScore(models.Model):
         _years = [2020, 2021, 2022]
         for _year in _years:
             _records = self._load_year(_year, _provinces_dict, _types_dict)
+            _logger.info("oneone section load year: %s" % _year)
+            _logger.info(_records)
             for _record in _records:
                 self.env["gaokao.oneone_section"].create(_record)
         return
